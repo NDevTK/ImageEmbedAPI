60668643008918a57a2afa80627d86f7c477f57f
NDevTK <31563761+NDevTK@users.noreply.github.com>
2020-12-06 18:10:34 +0000

    Because crypto.getRandomValues

--- index.js
+++ index.js
@@ -30,10 +30,31 @@ async function API(subject, count = 1, dateupload = 0) {
 }
 
 function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+  const range = max - min;
+  const maxGeneratedValue = 0xFFFFFFFF;
+  const possibleResultValues = range + 1;
+  const possibleGeneratedValues = maxGeneratedValue + 1;
+  const remainder = possibleGeneratedValues % possibleResultValues;
+  const maxUnbiased = maxGeneratedValue - remainder;
+
+  if (!Number.isInteger(min) || !Number.isInteger(max) ||
+       max > Number.MAX_SAFE_INTEGER || min < Number.MIN_SAFE_INTEGER) {
+    throw new Error('Arguments must be safe integers.');
+  } else if (range > maxGeneratedValue) {
+    throw new Error(`Range of ${range} (from ${min} to ${max}) > ${maxGeneratedValue}.`);
+  } else if (max < min) {
+    throw new Error(`max (${max}) must be >= min (${min}).`);
+  } else if (min === max) {
+    return min;
+  } 
+
+  let generated;
+  do {
+    generated = crypto.getRandomValues(new Uint32Array(1))[0];
+  } while (generated > maxUnbiased);
+
+  return min + (generated % possibleResultValues);
+};
 
 async function getURL(subject) {
     var count = 1;
