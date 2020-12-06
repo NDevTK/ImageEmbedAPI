2e11180d1a90e1495ca0577f012d39fe853b69ca
NDevTK <31563761+NDevTK@users.noreply.github.com>
2020-12-06 17:48:51 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
+    event.respondWith(handleRequest(event.request))
 })
 
 const key = "CorgiGoesHere";
 const limit = 4001;
 
 async function handleRequest(request) {
-  let { searchParams } = new URL(request.url);
-  let subject = searchParams.get("subject");
-  if(subject === null || typeof subject !== 'string' || subject.length > 10) {
-      return new Response("Subject is not valid", {status: 400})
-  }
-  return getURL(encodeURIComponent(subject));
+    let { searchParams } = new URL(request.url);
+    let subject = searchParams.get("subject");
+    if (subject === null || typeof subject !== 'string' || subject.length > 10) {
+        return new Response("Subject is not valid", {
+            status: 400
+        })
+    }
+    return getURL(encodeURIComponent(subject));
 }
 
 async function API(subject, count = 1, dateupload = 0) {
-  let data = await fetch("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key="+key+"&format=json&in_gallery=1&media=photos&per_page=1&extras=url_o,date_upload&license=9&nojsoncallback=1&max_upload_date="+dateupload+"&page="+count+"&text="+subject);
-  return await data.json()
+    let data = await fetch("https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + key + "&format=json&in_gallery=1&media=photos&per_page=500&extras=url_o,date_upload&license=9&nojsoncallback=1&max_upload_date=" + dateupload + "&page=" + count + "&text=" + subject, {
+        cf: {
+            cacheTtlByStatus: {
+                "200-299": 86400,
+                404: 1,
+                "500-599": 0
+            }
+        },
+    });
+    return await data.json()
 }
 
 function getRandomInt(min, max) {
@@ -27,39 +37,48 @@ function getRandomInt(min, max) {
 
 async function getURL(subject) {
     var count = 1;
-	try {
+    try {
         var result = await API(subject, count);
-		if(result.stat !== "ok") return new Response("Remote error", {status: 502});
-		var pages = result.photos.pages;
-		if(pages < 1) return new Response("Subject not found", {status: 404});
+        if (result.stat !== "ok") return new Response("Remote error", {
+            status: 502
+        });
+        var pages = result.photos.pages;
+        if (pages < 1) return new Response("Subject not found", {
+            status: 404
+        });
     } catch {
-        return new Response("API error", {status: 400});
+        return new Response("API error", {
+            status: 400
+        });
     }
 
-    count = Math.floor(getRandomInt(1, pages));
+    count = getRandomInt(1, pages);
     var result = await getPhoto(subject, count);
     return new Response("", {
         status: "307",
         headers: {
-            "Location": result.photos.photo[0].url_o,
-            "Access-Control-Allow-Origin": "*"
+            "Location": result.url_o,
+            "Access-Control-Allow-Origin": "*",
+            "Cache-Control": "max-age=1"
         }
     });
 }
 
 async function getPhoto(subject, count = 1) {
-    var requests = Math.floor(count/limit);
+    var requests = Math.floor(count / limit);
     // Flickr API limit workaround.
     var index = (requests > 0) ? (count - limit * requests) + 1 : count;
-	var dateupload = 0;
-	for (requests < 0; requests--;) {
-		let result = await API(subject, limit, dateupload);
-		if(result.stat !== "ok") continue
-		try {
-			dateupload = result.photos.photo[0].dateupload;
-		} catch {
-			continue
-		}
-	}
-	return await API(subject, index, dateupload);
+    var dateupload = 0;
+    for (requests < 0; requests--;) {
+        let result = await API(subject, limit, dateupload);
+        if (result.stat !== "ok") continue
+        try {
+            dateupload = result.photos.photo[0].dateupload;
+        } catch {
+            continue
+        }
+    }
+    var result = await API(subject, index, dateupload);
+    var count2 = getRandomInt(1, result.photos.photo.length);
+    return result.photos.photo[count2 - 1];
 }
